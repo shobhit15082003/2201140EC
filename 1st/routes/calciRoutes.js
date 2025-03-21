db1f8b9d285e7033f1a6cdbfa53cc00ea7a853da
shobhit15082003 <shobhit.2201140ec@iiitbh.ac.in>
2025-03-21 18:45:14 +0530

    routes created

---
+++ 1st/routes/calciRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/calciControllers');
+
+router.get('/:type',controller.getNum);
+
+module.exports=router;
